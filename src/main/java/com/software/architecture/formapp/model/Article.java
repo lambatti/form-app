aaf732ea25046ff01aa686626316dd9bcc382cfa
lambatti <adamgrzegorzgala@gmail.com>
2022-01-04 19:56:55 +0100

    Added Article model, updated Author model to include join table

---
+++ src/main/java/com/software/architecture/formapp/model/Article.java
@@ -0,0 +1,31 @@
+package com.software.architecture.formapp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "articles")
+@Getter
+@Setter
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int articleId;
+
+    @ManyToMany
+    @JoinTable(
+            name="articles_authors",
+            joinColumns = @JoinColumn(name="article_id"),
+            inverseJoinColumns = @JoinColumn(name="author_id")
+
+    )
+    private List<Author> authors;
+
+    @ManyToMany
+    private List<Author> reviewers;
+
+}
--- src/main/java/com/software/architecture/formapp/model/Author.java
+++ src/main/java/com/software/architecture/formapp/model/Author.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "authors")
@@ -13,10 +14,19 @@ public class Author {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private int authorId;
 
     private String firstName;
     private String lastName;
     private String email;
     private String affiliation;
+
+    @ManyToMany
+    @JoinTable(
+            name="articles_authors",
+            joinColumns = @JoinColumn(name="author_id"),
+            inverseJoinColumns = @JoinColumn(name="article_id")
+
+    )
+    private List<Article> articles;
 }
